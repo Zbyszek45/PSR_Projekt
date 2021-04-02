c72490148c6fd9a1ac0c3f210d8536068fc7cedb
Michał Buczek <michal.buczek@protonmail.com>
2021-04-02 14:44:04 +0200

    All tests passed

--- ComparatorServer/Server.cs
+++ ComparatorServer/Server.cs
@@ -10,15 +10,7 @@ namespace ComparatorServer
     {
         static void Main(string[] args)
         {
-            var comparator = new Comparator(patternLength: 5, caseSensitive: true);
-            string source = "program";
-            string target = "programowanie";
-
-            var expectedResults = new List<string>() { "program" };
-            //var result = comparator.Compare(source, target);
-
-            comparator.Compare(source, target);
-            Console.ReadLine();
+            
         }
     }
 }
